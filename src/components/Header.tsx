b26e7c96dbef8378ef5d91fed2eb86c0b0255d52
Nilson Nascimento <nilson@chuva.io>
2025-05-11 21:47:57 -0100

    Enable dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { SignInButton, SignUpButton } from "@clerk/clerk-react";
 
 export default function Header() {
   return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
+    <header className="p-2 flex gap-2 justify-between">
       <nav className="flex flex-row">
         <div className="px-2 font-bold">
           <Link to="/">Home</Link>
